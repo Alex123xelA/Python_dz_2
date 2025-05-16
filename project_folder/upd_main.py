6b133ed9fd5c210d5c76e2747ebc74b34ff30992
Anna <lenkovaana9@gmail.com>
2025-05-16 22:06:52 +0300

    Create upd_main.py

---
+++ project_folder/upd_main.py
@@ -0,0 +1,33 @@
+from db_loader import load_excel_to_pickle, select_dataframe
+from generate_reports import *
+
+def main():
+    print("Загрузка базы данных и создание справочников...")
+    load_excel_to_pickle()
+
+    df = select_dataframe()
+
+    print("Выберите действие:\n1. Создать текстовый отчет\n2. Построить график\n3. Круговая диаграмма")
+    choice = input("Ваш выбор: ")
+
+    if choice == "1":
+        col = input("Введите колонку фильтра: ")
+        val = input("Введите значение: ")
+        selected_cols = input("Введите через запятую нужные колонки: ").split(", ")
+        report = report_filter_by_criteria(df, {col: val}, selected_cols)
+        print(report)
+
+    elif choice == "2":
+        x = input("Введите X (независимая переменная): ")
+        y = input("Введите Y (зависимая переменная): ")
+        plot_price_vs_quantity(df, x, y)
+
+    elif choice == "3":
+        col = input("Введите имя колонки для круговой диаграммы: ")
+        plot_pie_chart_by_column(df, col)
+
+    elif choice == "4":
+        create_pivot_table(df)
+
+if __name__ == "__main__":
+    main()
